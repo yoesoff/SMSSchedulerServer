a6502b988e884a9eb2008d32266f578b35583761
Yusuf T420 <mhyusufibrahim@gmail.com>
2021-12-14 10:40:23 +0700

    Added faker and seeders data

---
+++ seeders/20211214032436-users.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const faker = require('faker');
+
+
+const users = [...Array(200)].map((user) => (
+    {
+        name: faker.name.firstName(),
+        email: faker.internet.email(),
+        phone: faker.phone.phoneNumber(),
+        password: faker.internet.password(8),
+        createdAt: new Date(),
+        updatedAt: new Date()
+    }
+))
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        return queryInterface.bulkInsert('Users', users, {});
+    },
+
+    down: async (queryInterface, Sequelize) => {
+        return queryInterface.bulkDelete('Users', null, {});
+    }
+};
---
+++ seeders/20211214032519-schedules.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const faker = require('faker');
+
+const schedules = [...Array(3)].map((user) => (
+    {
+      run_at: new Date(),
+      message: faker.git.commitMessage(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+))
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Schedules', schedules, {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Schedules', null, {});
+  }
+};
